ab5a5c34eeaf2a1efe8055169158af1c423f3bfd
Fredrik Rubensson <fredrik.rubensson@eniro.com>
2018-04-09 17:50:22 +0200

    empty state of things

--- features/support/world.js
+++ features/support/world.js
@@ -5,19 +5,7 @@ class BankWorld {
     constructor() {
         this.accounts = {}
     }
-
-    addAccount(name) {
-        this.accounts[name] = 0
-    }
-
-    deposit(name, amount) {
-        this.accounts[name] += amount
-    }
-
-    addInterest(name, interest) {
-        var interestToAdd = this.accounts[name] * (interest / 100)
-        this.deposit(name, Math.round(interestToAdd))
-    }
+    
 }
 
 setWorldConstructor(BankWorld);
